c808d415a7e2b1e2cfa523078164247df3ddcb07
VicentiuM <m_vicentiu@yahoo.com>
2016-05-25 15:16:16 +0300

    Added some predefined tutorials

--- server/server2/server.php
+++ server/server2/server.php
@@ -1,6 +1,32 @@
 <?php
 
-echo file_get_contents("files/hello.js");
+if (strcmp($_POST["code"], "") !== 0)
+	echo file_get_contents("files/hello.js");
+
+switch( $_POST["code-tutorial"] ) {
+	case 1:
+		echo file_get_contents("tutorials/ex010_hello.ceu");
+		break;
+	case 2:
+		echo file_get_contents("tutorials/ex020_events.ceu");
+		break;
+	case 3:
+		echo file_get_contents("tutorials/ex030_parand.ceu");
+		break;
+	case 4:
+		echo file_get_contents("tutorials/ex040_paror.ceu");
+		break;
+	case 5:
+		echo file_get_contents("tutorials/ex050_term.ceu");
+		break;
+	case 6:
+		echo file_get_contents("tutorials/ex060_par.ceu");
+		break;
+	case 7:
+		echo file_get_contents("tutorials/ex070_AB.ceu");
+		break;
+}
+
 //include 'library.php';
 //create_file($_POST["name"]);
 //$filename = uniqid('CEU', true) . '.ceu';
