a6974a53a5fba86451761526514e7a71c42acd46
VicentiuM <m_vicentiu@yahoo.com>
2016-05-31 16:06:29 +0300

    made server smaller

--- client.js
+++ client.js
@@ -1,3 +1,8 @@
+var tutorial = ["tutorials/ex010_hello.ceu", "tutorials/ex020_events.ceu", "tutorials/ex030_parand.ceu",
+				"tutorials/ex040_paror.ceu", "tutorials/ex050_term.ceu", "tutorials/ex060_par.ceu",
+				"tutorials/ex070_AB.ceu"];
+
+
 function compile_code() {
 
 	var send = document.getElementById('code').value;
@@ -28,15 +33,15 @@ function compile_code() {
 var nr = 0;
 function inc_nr() {
 	nr = nr + 1;
-	if (nr == 8)
-		nr = 1;
+	if (nr == 7)
+		nr = 0;
 	get_tutorial()
 }
 
 function dec_nr() {
 	nr = nr - 1;
-	if (nr <= 0)
-		nr = 7;
+	if (nr < 0)
+		nr = 6;
 	get_tutorial()
 }
 
@@ -46,11 +51,12 @@ function set_nr(x) {
 }
 
 function get_tutorial() {
-	$.ajax({ type: "POST",   
-		url: "server.php",
-		data: { 'code-tutorial' : nr }
+	$.ajax({ type: "GET",   
+		url: tutorial[nr]
 	}).done(function(text) {
 		document.getElementById("code").value = text;
 	});
 
 }
+
+get_tutorial();
--- server.php
+++ server.php
@@ -3,48 +3,4 @@
 if (strcmp($_POST["code"], "") !== 0)
 	echo file_get_contents("files/hello.js");
 
-switch( $_POST["code-tutorial"] ) {
-
-	case 1:
-		echo file_get_contents("tutorials/ex010_hello.ceu");
-		break;
-	case 2:
-		echo file_get_contents("tutorials/ex020_events.ceu");
-		break;
-	case 3:
-		echo file_get_contents("tutorials/ex030_parand.ceu");
-		break;
-	case 4:
-		echo file_get_contents("tutorials/ex040_paror.ceu");
-		break;
-	case 5:
-		echo file_get_contents("tutorials/ex050_term.ceu");
-		break;
-	case 6:
-		echo file_get_contents("tutorials/ex060_par.ceu");
-		break;
-	case 7:
-		echo file_get_contents("tutorials/ex070_AB.ceu");
-		break;
-
-}
-
-
-//include 'library.php';
-//create_file($_POST["name"]);
-//$filename = uniqid('CEU', true) . '.ceu';
-//echo $filename;
-//$fp = fopen("/home/vic/git/Ceu-Emscripten/server/server2/fisier.txt", "wb");
-//fwrite($fp, $_POST["name"]);
-//fclose($fp);
-//$file = tempnam("/tmp/HA/", 'CEU');
-//file_put_contents($file.'.ceu', $_POST["name"]);
-//{
-   //use your file
-	//exec('./a.out /tmp/'.$file.'.ceu /home/vic/git/'.$file.'.ceu', $out);
-//exec('./a.out heyo.txt', $out);
-//print_r($out);
-//}
-//unlink($file);//to delete an empty file that tempnam creates
-//unlink($file.'.extension');//to delete your file
 ?>
